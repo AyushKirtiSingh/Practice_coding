c6aa30c4bd43d3645e10ef098d6fa8470283a5b4
ayushsingh9904@gmail.com <muskansingh@Muskans-MacBook-Air.local>
2026-02-22 09:42:29 -0800

    literals

---
+++ literals.java
@@ -0,0 +1,17 @@
+public class literals {
+
+    public static void main(String[] args) {
+        byte age = 34;
+        int age1 = 56;
+        short age2 = 89;
+        long age3 = 34444444L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;        //In float default value is double so we don't have to write d.
+        boolean a = true;
+        System.out.print(age);
+        String str = "harry";
+        System.out.println(str);
+
+    }
+}
