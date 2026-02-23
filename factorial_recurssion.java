498c16242ba386c172c17caaf96d74fe59acdab3
ayushsingh9904@gmail.com <muskansingh@Muskans-MacBook-Air.local>
2026-02-23 13:54:47 -0800

    factorial using recurssion

---
+++ factorial_recurssion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class factorial_recurssion {
+    static int factorial(int n) {
+        if(n==0 || n==1){
+        return 1;
+
+        }
+        else{
+          return n*factorial(n-1);
+
+        }
+    }
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  a number: ");
+        n = sc.nextInt();
+        int result = factorial(n);
+        System.out.println(result);
+    }
+    
+}
