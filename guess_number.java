ba70d4251408cd9be270d59228b0296dd3b95c69
ayushsingh9904@gmail.com <muskansingh@Muskans-MacBook-Air.local>
2026-02-24 11:51:05 -0800

    small project 2

---
+++ guess_number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class guess_number {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;  // Number between 1–100
+        int guess = 0;
+
+        System.out.println("Guess the Number Game!");
+        System.out.println("I'm thinking of a number between 1 and 100");
+
+        while(guess != number) {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
